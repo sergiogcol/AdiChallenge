e3557878ccb9b92652ae22f1a4f38c9c032a2876
sergiogconvers <sergiogconvers@gmail.com>
2019-12-16 11:38:42 -0500

    last commit before dividing cart component into smaller components

--- src/Components/AuxiliaryComponents/SingleSelect.js
+++ src/Components/AuxiliaryComponents/SingleSelect.js
@@ -1,4 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+const SelectContainer = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 35%;
+    text-transform: uppercase;
+    cursor: pointer;
+    height: 50px;
+    padding: 0 20px;
+    border: 1px solid #767677;
+`
+const Select = styled.select`
+    -moz-appearance:none; /* Firefox */
+    -webkit-appearance:none; /* Safari and Chrome */
+    appearance:none;
+    box-sizing: border-box;
+    border:none;
+    text-align: center;
+    font-size: 14px;
+    letter-spacing: 2px;
+    font-weight: 700;
+    width: 100%;
+`
 
 export default class SingleSelect extends Component {
     state = {}
@@ -12,9 +40,13 @@ export default class SingleSelect extends Component {
     render() {
         let { options } = this.props
         return (
-            <select onChange={(event) => this.addSelected(event.target.value)}>
-                {options.map((option, i) => <option key={i} value={option.value}>{option.label}</option>)}
-            </select>
+            <SelectContainer>
+            <Select onChange={(event) => this.addSelected(event.target.value)}>
+                {options.map((option, i) => <option key={i} value={option.value}>{option.label}</option> )}
+            </Select>
+            <FontAwesomeIcon icon={faChevronDown} />
+            </SelectContainer>
         )
     }
 }
+//<FontAwesomeIcon icon={faChevronDown} />
--- src/Components/Checkout/Cart.js
+++ src/Components/Checkout/Cart.js
@@ -5,13 +5,20 @@ import productImage from '../../Images/superstar_cloud_white.webp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import SingleSelect from '../AuxiliaryComponents/SingleSelect';
 
 const GlobalStyle = createGlobalStyle`
+  html{
+    height: 100vh;
+    width: 100vw;
+  }
   body {
+    box-sizing: border-box;
     margin: 2%;
     font-size: 16px;
-    height: 100vh;
     text-transform: uppercase;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 `
 const PageTitleContainer = styled.header`
@@ -19,6 +26,7 @@ const PageTitleContainer = styled.header`
   justify-content: space-between;
   align-items: center; 
   box-sizing: border-box;
+  width: 100%;
   margin: 0px;
   padding: 0px;
 `
@@ -50,18 +58,49 @@ const TotalTitle = styled.p`
     text-transform: uppercase
   }
 `
-const ProductSummary = styled.div`
+const ContentContainer = styled.section`
+  display: flex;
+  width:100%;
+`
+const OrderSummaryContainer = styled.div`
+  margin-left: 5%;
+  width: 30%
+`
+const OrderSummary = styled.article`
+  border: 1px solid #ebedee;
+  padding: 24px 15px;
+  margin-top: 40px;
+`
+const OrderSummaryData = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+  font-size: 14px;
+  line-height: 20px;
+  & p{
+    margin: 0;
+  margin-bottom: 14px;
+  }
+  &.total{
+    font-weight: 700;
+  }
+`
+const ProductSummary = styled.section`
   box-sizing: border-box;
   border: 1px solid black;
-  width: 60%;
-  height: 20%;
+  min-width:60%;
+  height: 242px;
   margin-top: 40px;
   display:flex;
+  justify-content: space-between;
 `
-const ProductAndPriceContainer = styled.div`
+const ProductAndPriceContainer = styled.article`
   display: flex;
   flex-direction: column; 
-  padding-left:2%;
+  padding-left:8%;
+  padding-bottom: 16px;
+  height: 100%;
+ 
   & > p {
     margin: 0px;
   }
@@ -70,19 +109,122 @@ const ProductAndPriceContainer = styled.div`
   }
 `
 const IconContainer = styled.div`
-  width: 25%; 
+  width: 20%; 
   margin-top: 15px; 
-  font-size: 20px; 
   display: flex; 
   flex-direction: column; 
   align-items: center; 
   height: 30%;
   justify-content: space-between;
+  & > button {
+    border: none;
+    background-color: white;
+    font-size: 20px; 
+    &:hover{
+      cursor: pointer;
+      color: grey
+    }
+  }
+`
+const CheckoutButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top:40px;
+  width: 100%;
+  min-width:560px;
+  &.column{
+    flex-direction: column;
+    min-width: 100%;
+  }
+`
+const CheckoutButton = styled.button`
+  flex: 1;
+  font-size: 0.8rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  transform: translate(-3px,-3px);
+  letter-spacing: 2px;
+  height: 50px;
+  justify-content: space-between;
+  background-color: black;
+  color: white;
+  align-items: center;
+  border: none;
+  border-radius: 0;
+  cursor: pointer;
+  display: inline-flex;
+  line-height: 50px;
+  min-height: 50px;
+  padding: 0 21px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: transform .1s cubic-bezier(.3,0,.45,1),color .1s cubic-bezier(.3,0,.45,1),border .1s cubic-bezier(.3,0,.45,1),background .1s cubic-bezier(.3,0,.45,1);
+  white-space: nowrap;
+  margin: 0;
+  &::before{
+    border-bottom: 1px solid #000;
+    border-left: 1px solid #000;
+    bottom: -3px;
+    height: 3px;
+    left: 3px;
+    width: 100%;
+    content: "";
+    display: block;
+    position: absolute;
+    transition: .1s cubic-bezier(.3,0,.45,1);
+  }
+  &::after{
+    border-right: 1px solid #000;
+    border-top: 1px solid #000;
+    height: 100%;
+    right: -3px;
+    top: 3px;
+    width: 3px;
+  }
+  &::after, &::before{
+    content: "";
+    display: block;
+    position: absolute;
+    transition: .1s cubic-bezier(.3,0,.45,1);
+  }
+  &.paypal{
+    background-color:white;
+    color:black;
+    border:1px solid #000;
+    line-height: 48px;
+    &::before{
+      margin-left:21px;
+      background: url(https://adl-foundation.adidas.com/dev/rc-19.0.0/assets/img/shared/icon-paypal-logo.svg) no-repeat 0;
+      content: "";
+      display: inline-block;
+      height: 48px;
+      min-width: 100px;
+      width: 100%;
+      border: none;
+    }
+    &::after{
+      border: none;
+    }
+  }
+`
+const CheckoutSpan = styled.span`
+  flex: 1 1 auto;
+`
+const OrSeparator = styled.p`
+  margin: 0 14px;
+  line-height: 48px;
+  text-align: center;
 `
 
 // http://testingfreak.com/how-to-fix-cross-origin-request-security-cors-error-in-firefox-chrome-and-ie/
 export default class Cart extends Component {
 
+  addSelected = (property, value) => {
+    this.setState({
+      [property]: value
+    })
+  }
   render() {
     return (
       <React.Fragment>
@@ -96,24 +238,86 @@ export default class Cart extends Component {
           :(1 item)
           <strong> $80</strong>
         </TotalTitle>
+        <ContentContainer>
+        <div>
         <ProductSummary>
-          <img src={productImage} alt='Adidas Superstar Shoes Cloud White Color' height='100%' width='50%'></img>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <img src={productImage} alt='Adidas Superstar Shoes Cloud White Color' height='240px' width='240px'></img>
+          <div style={{ display: 'flex', justifyContent: 'space-between', height: '100%', width: '100%' }}>
             <ProductAndPriceContainer>
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <p>Superstar Shoes</p>
-                <p>$80.0</p>
+                <p>$80.00</p>
               </div>
-              <p>Cloud White / Core Black / Cloud White</p>
+              <p style={{ textOverflow: 'ellipsis' }}>Cloud White / Core Black / Cloud White</p>
               <p className='addSpace'>Size: M 5 / W 6</p>
-              <p><strong>In Stock</strong></p>
+              <p style={{ marginBottom: '10%' }}><strong>In Stock</strong></p>
+              <SingleSelect options={[{ value: null, label: 1 }]} addSelected={this.addSelected} property={'selectedQuantity'} />
             </ProductAndPriceContainer>
             <IconContainer>
-              <FontAwesomeIcon icon={faTimes} />
-              <FontAwesomeIcon icon={faHeart} />
+              <button>
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faHeart} />
+              </button>
             </IconContainer>
           </div>
         </ProductSummary>
+        <CheckoutButtonContainer>
+          <CheckoutButton>
+            <CheckoutSpan>checkout</CheckoutSpan>
+            <div style={{ fontSize: '20px' }}>
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </div>
+          </CheckoutButton>
+          <OrSeparator>OR</OrSeparator>
+          <CheckoutButton className='paypal'>
+            <CheckoutSpan></CheckoutSpan>
+            <div style={{ fontSize: '20px' }}>
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </div>
+          </CheckoutButton>
+        </CheckoutButtonContainer>
+        </div>
+        <OrderSummaryContainer>
+        <CheckoutButtonContainer className='column'>
+          <CheckoutButton>
+            <CheckoutSpan>checkout</CheckoutSpan>
+            <div style={{ fontSize: '20px' }}>
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </div>
+          </CheckoutButton>
+          <OrSeparator>OR</OrSeparator>
+          <CheckoutButton className='paypal'>
+            <CheckoutSpan></CheckoutSpan>
+            <div style={{ fontSize: '20px' }}>
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </div>
+          </CheckoutButton>
+        </CheckoutButtonContainer>
+        <OrderSummary>
+          <div>
+            <h5 style={{fontSize: '18px', lineHeight: '16px', margin: '0',marginBottom: '20px'}}>ORDER SUMMARY</h5>
+            <OrderSummaryData>
+              <p>1 item</p>
+              <p>$80.00</p>
+            </OrderSummaryData>
+            <OrderSummaryData>
+              <p>delivery</p>
+              <p>free</p>
+            </OrderSummaryData>
+            <OrderSummaryData>
+              <p>sales tax</p>
+              <p>-</p>
+            </OrderSummaryData>
+            <OrderSummaryData className='total'>
+              <p>total</p>
+              <p>$80.00</p>
+            </OrderSummaryData>
+          </div>
+        </OrderSummary>
+        </OrderSummaryContainer>
+        </ContentContainer>
       </React.Fragment>
     );
   }
